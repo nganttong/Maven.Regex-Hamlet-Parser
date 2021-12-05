6864c7b3cbb7f37450cd3bc842f5beb350bbabb9
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-05 17:49:22 -0500

    whill this work, i need to test :/

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -34,6 +36,18 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public Boolean containsPattern(String file, String pattern){
+        Pattern containsPattern = Pattern.compile(pattern);
+        return containsPattern.matcher(file).find();
+    }
+
+    public String switchNames(String file, String pattern, String name){
+        Pattern containsPattern = Pattern.compile(pattern);
+        Matcher matchThis = containsPattern.matcher(file);
+        String replacedText = matchThis.replaceAll(name);
+        return replacedText;
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -15,6 +15,7 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+
     }
 
     @Test
